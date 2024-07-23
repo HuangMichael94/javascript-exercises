a4056cf651f5ce61cf8616bedc6538054e788c40
HuangMichael94 <mshuang2001@gmail.com>
2024-07-23 10:12:54 -0700

    part 8

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,41 @@
-const add = function() {
-	
+const add = function(num1, num2) {
+	return num1+num2;
 };
 
-const subtract = function() {
-	
+const subtract = function(num1,num2) {
+	return num1-num2;
 };
 
-const sum = function() {
-	
+const sum = function(args) {
+  returnTotal = 0;
+  args.forEach(element => {
+    returnTotal += element;
+  });
+  return returnTotal;
 };
 
-const multiply = function() {
-
+const multiply = function(args) {
+  returnTotal = 1;
+  for (let i = 0; i < args.length; i++) {
+    returnTotal *= args[i];
+  }
+  return returnTotal;
 };
 
-const power = function() {
-	
+const power = function(num1, num2) {
+  returnTotal = 1;
+  for (let i = 0; i < num2; i++) {
+    returnTotal *= num1;
+  }
+  return returnTotal;
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+  returnTotal = 1;
+	for (let i = num; i > 0; i--) {
+    returnTotal *= i;
+  }
+  return returnTotal;
 };
 
 // Do not edit below this line
