0045707975d07ad4940815a5c9435b8199d2adfe
HuangMichael94 <mshuang2001@gmail.com>
2024-07-22 22:48:32 -0700

    part2

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,13 @@
-const repeatString = function() {
+const repeatString = function(str, num) {
+    if (num < 0) {
+        return 'ERROR';
+    }
 
+    returnString = "";
+    for (let i = 0; i < num; i++) {
+        returnString += str;
+    }
+    return returnString;
 };
 
 // Do not edit below this line
