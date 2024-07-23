4ca78219abbcb08a27027cfa46d8d423328ac2a9
HuangMichael94 <mshuang2001@gmail.com>
2024-07-22 22:54:00 -0700

    part 2

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,12 @@
-const reverseString = function() {
+const reverseString = function(str) {
+    chars = str.split("");
+    returnString = "";
 
+    for (let i = chars.length-1; i > -1; i--){
+        returnString += chars[i];
+    }
+
+    return returnString;
 };
 
 // Do not edit below this line
