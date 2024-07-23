51312d269c56c006c0f2446841bef029e77daa0b
HuangMichael94 <mshuang2001@gmail.com>
2024-07-23 16:54:50 -0700

    part 11

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,26 @@
-const findTheOldest = function() {
+const findTheOldest = function(args) {
+    let person = args[0]
+    let oldestAge = 0;
 
+    for (let i = 0; i < args.length; i++) {
+        if (!args[i].yearOfDeath) {
+            age = 2024 - args[i].yearOfBirth;
+
+            if (age > oldestAge) {
+                person = args[i];
+                oldestAge = args[i].yearOfDeath-args[i].yearOfBirth;
+            }
+        }
+
+
+        else {
+            if (args[i].yearOfDeath-args[i].yearOfBirth > oldestAge) {
+                person = args[i];
+                oldestAge = args[i].yearOfDeath-args[i].yearOfBirth;
+            }
+        }
+    }
+    return person;
 };
 
 // Do not edit below this line
